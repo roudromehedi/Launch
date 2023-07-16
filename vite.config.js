a211b30ae930c9297659ec4f914b791323a82ada
RoudroMehedi <roudro.m@gmail.com>
2023-07-16 16:13:00 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        'images/person_1.jpg',
+        // Add any other external modules here
+        'images/market-launch-pana.svg',
+        'images/dashboard.jpg'
+      ],
+    },
+  },
+});
